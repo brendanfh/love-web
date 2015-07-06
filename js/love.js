5fbbc7742cdd1cc4951fd25cbe5acb8593e90f28
Brendan <brendan.f.hansen@gmail.com>
2015-07-06 16:22:46 +0000

    adding basic files

---
+++ js/love.js
@@ -0,0 +1,21 @@
+Love = (function() {
+    function Love() {
+        this.graphics = new Love.Graphics()   
+    }
+    
+    Love.prototype.load = function() {
+    };
+    
+    return Love;
+})();
+
+Love.Graphics = (function() {
+    function Graphics() {
+        
+    }
+    
+    Graphics.prototype.arc = function() {
+    };
+    
+    return Graphics;
+})();
